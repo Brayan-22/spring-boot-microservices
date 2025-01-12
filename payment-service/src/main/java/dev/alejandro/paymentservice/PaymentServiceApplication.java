198a36f0d15a97d182ef2c6efc3333d321fd795c
alejandro <alejandroriveros100@gmail.com>
2025-01-11 20:14:18 -0500

    [Add] docker stack deployment

--- payment-service/src/main/java/dev/alejandro/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/dev/alejandro/paymentservice/PaymentServiceApplication.java
@@ -1,11 +1,14 @@
 package dev.alejandro.paymentservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@OpenAPIDefinition(info = @Info(title = "Payment API",version = "1.0", description = "Documentation Payment API v1.0"))
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
